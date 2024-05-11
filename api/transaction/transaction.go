106eb338862ddb190aa7dc3ea15953ff22d23398
Witthawat Leethong <witthawat@widelynext.co.th>
2024-05-11 15:29:33 +0700

    modify transaction struct

--- api/transaction/transaction.go
+++ api/transaction/transaction.go
@@ -2,7 +2,17 @@ package transaction
 
 import "time"
 
-type Transaction struct {
+type TransactionRequest struct {
+	Date            time.Time `json:"date"`
+	Amount          float64   `json:"amount"`
+	Category        string    `json:"category"`
+	TransactionType string    `json:"transaction_type"`
+	Note            string    `json:"note"`
+	ImageUrl        string    `json:"image_url"`
+}
+
+type TransactionResponse struct {
+	ID              int64     `json:"id"`
 	Date            time.Time `json:"date"`
 	Amount          float64   `json:"amount"`
 	Category        string    `json:"category"`
